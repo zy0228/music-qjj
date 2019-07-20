fc3412a1822c5f45e72d7a0315b6e2470141ad81
zy0228 <2472305432@qq.com>
2019-07-20 18:19:11 +0800

    <singer> fix singer data

--- src/views/singer/singer.vue
+++ src/views/singer/singer.vue
@@ -5,19 +5,66 @@
 <script>
 import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 
 export default {
   data () {
     return {
+      singerData: []
     }
   },
   methods: {
     fetch() {
       getSingerList().then(res => {
         if (res.code === ERR_OK) {
-          console.log(res.data)
+          let singers = res.data.list
+          this.singerData = this._normalizeSinger(singers)
+          console.log(this.singerData)
         }
       })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          name: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            name: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 根据需求处理下顺序
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.name.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.name === HOT_NAME) {
+          hot.push(val)
+        }
+        ret.sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0))
+      }
+      return hot.concat(ret)
     }
   }
 }
